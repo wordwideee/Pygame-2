ed1074f8640748086d2cd4c14a5caba53270802d
wordwideee <dominowawrykiewicz@gmail.com>
2022-05-30 13:49:18 +0200

    Dodanie skakania

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((800, 800))
+win = pygame.display.set_mode((1000, 500))
 
 pygame.display.set_caption("Ciekawa Gierka")
 
-x = 400
-y = 400
+x = 250
+y = 250
 radius = 15
-vel = 10
+vel_x = 10
+vel_y = 10
+jump = False
 
 run = True
 while run:
 
-    win.fill((0, 0, 0))
+    win.fill((255, 255, 255))
 
-    pygame.draw.circle(win, (50, 135, 168), (int(x), int(y)), radius)
+    pygame.draw.circle(win, (0, 0, 0), (int(x), int(y)), radius)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -25,17 +27,21 @@ while run:
     userInput = pygame.key.get_pressed()
 
     if userInput[pygame.K_a] and x > 0:
-        x -= vel
+        x -= vel_x
 
-    if userInput[pygame.K_d] and x < 800:
-        x += vel
+    if userInput[pygame.K_d] and x < 1000:
+        x += vel_x
 
-    if userInput[pygame.K_w] and y > 0:
-        y -= vel
+    if jump is False and userInput[pygame.K_w]:
+        jump = True
 
-    if userInput[pygame.K_s] and y < 800:
-        y += vel
+    if jump is True:
+        y -= vel_y*4
+        vel_y -= 1
+        if vel_y < -10:
+            jump = False
+            vel_y = 10
 
-    pygame.time.delay(15)
+    pygame.time.delay(20)
 
     pygame.display.update()
